4564c411f31392c645bd415ca7e24984a4c21953
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-03-22 19:15:49 -0300

    Add files via upload

---
+++ gerente.java
@@ -0,0 +1,12 @@
+package contamelhor;
+
+public class gerente {
+        String numerocriado;
+        String agenciacriado;
+
+boolean CriarConta(double Nomecliente){
+    numerocriado = "12345";
+    agenciacriado = "1234";
+    return true;
+}        
+}
