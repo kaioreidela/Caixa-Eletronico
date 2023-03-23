000b188e04da7bf6419202f1537b20c3dbc3e305
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-03-23 19:12:23 -0300

    com verficacao

--- testecorrente.java
+++ testecorrente.java
@@ -1,16 +1,19 @@
 package contamelhor;
+ipackage contamelhor;
 import java.util.Scanner;
 public class cliente {
   public static void main(String[] args) {
       conta conta = new conta();
       gerente gerente =new gerente();
       conta.saldo = 500;
+      boolean verificar = gerente.CriarConta(0);
+      
       System.out.println("seja bem vindo");
           Scanner entrada = new Scanner(System.in);
           System.out.println("Caixa eletronico:\n"
           + "vc é gerente ou cliente\n"
-          + "Digite 1 para: Cliente \n"
-          + "Digite 2 para: Gerente \n"
+          + "Digite 1 para: Gerente \n"
+          + "Digite 2 para: Cliente  \n"
           + "Digite 3 para: Sair \n");
           int opcao = entrada.nextInt();
           switch(opcao){
@@ -19,43 +22,57 @@ public class cliente {
               System.out.println("criar conta em nome de:");
               double Nomecliente = entrada10.nextInt();
               gerente.CriarConta( Nomecliente);
-              System.out.println("Numero da conta: "+ gerente.numerocriado
+              System.out.println("Senhor"+ gerente.nomedocliente +"aqui estão suas informações:\n"
+              +"Numero da conta: "+ gerente.numerocriado
               +"\nAgencia: "+ gerente.agenciacriado
               +"\nSaldo disponivel: "+ conta.saldo + " Reais");
-              break;
+              
 
 
 
-             case 2:
-              Scanner novaentrada = new Scanner(System.in);
-              System.out.println("Caixa eletronico:\n"
-              + "Digite 1 para: Depositar \n"
-              + "Digite 2 para: Sacar \n"
-              + "Digite 3 para: Sair \n");
-              int opcao2 = novaentrada.nextInt();
-              switch(opcao2){
-                case 1:
-                Scanner entrada100 = new Scanner(System.in);
-                System.out.println("quanto deseja depositar");
-                double valorDepositado = entrada100.nextInt();
-                conta.depositar(valorDepositado);
-                conta.consultarsaldo();
-                break;
-      
-                case 2:
-                Scanner entrada2 = new Scanner(System.in);
-                System.out.println("quanto deseja sacar");
-                double quantiaSacar  = entrada2.nextInt();
-                conta.realizarSaque(quantiaSacar);
-                conta.consultarsaldo();
-                break;
-      
-                case 3:
+              case 2:
+              System.out.println("POrfavor digite seu numero e agencia\n"
+              + "\n"
+              + "\n");
+              int a;
+              Scanner ler = new Scanner(System.in);
+              a = ler.nextInt();
+              
+
+
+              
+              if(verificar == true && a == gerente.agenciacriado){ 
+                System.out.println("Caixa eletronico:\n"
+                + "Digite 1 para: Depositar \n"
+                + "Digite 2 para: Sacar \n"
+                + "Digite 3 para: Sair \n");
+                Scanner novaentrada = new Scanner(System.in);
+                int opcao2 = novaentrada.nextInt();
+                         
+                switch(opcao2){
+                  case 1:
+                  Scanner entrada100 = new Scanner(System.in);
+                  System.out.println("quanto deseja depositar");
+                  double valorDepositado = entrada100.nextInt();
+                  conta.depositar(valorDepositado);
+                  conta.consultarsaldo();
                   break;
-                default:
-                  System.out.println("opcao digitada invalida");  
-          }  
+      
+                  case 2:
+                  Scanner entrada2 = new Scanner(System.in);
+                  System.out.println("quanto deseja sacar");
+                  double quantiaSacar  = entrada2.nextInt();
+                  conta.realizarSaque(quantiaSacar);
+                  conta.consultarsaldo();
+                  break;
+      
+                  case 3:
+                   break;
+                  default:
+                    System.out.println("opcao digitada invalida");  
+                }
+            }else{System.out.println("Conta inexistente");} 
          
-        }
-        }
- }
+          }
+          }
+   }
